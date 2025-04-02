c67d7978b9f2a1595151435fffb83c5c561bcbc7
Andreas Johansson <glanshammar70@gmail.com>
2025-04-02 11:41:38 +0200

    Updated main

--- main.py
+++ main.py
@@ -1,40 +1,54 @@
-from Data import UsersMain, DomainMain, Company, Consultant
+from Data import DomainMain, Company, Consultant
 from Agents import AgentManager, AgentType
 import requests
+import zmq
+from zmq.auth import load_certificate
+
+context = zmq.Context()
+client = context.socket(zmq.REQ)
+client.connect("tcp://server:5001")
+
 
 if __name__ == "__main__":
     while True:
         command = input(">> ")
+        if command.lower() == "consultant":
+            consult = Consultant.Generate()
+            print(consult.to_dict)
         if command.lower() == "consult":
-            consultant_dummy_data = {
-                "name": "Andreas Johansson",
-                "expertise": ["Project Management", "Agile Methodologies", "IT Consulting"],
-                "years_of_experience": 5,
-                "certifications": ["Programming Cert", "IT Cert", "Azure AI-900"],
-                "contact_email": "andreas@johansson.se",
-                "contact_phone": "+46 72 356 77 71"
+            consultant_dummy_data = Consultant.Generate()
+
+            command_data = {
+                'command': 'create',
+                'params': {
+                    'collection_name': 'Consultants',
+                    'document_data': consultant_dummy_data.to_dict()
+                }
             }
-            Consultant.AddConsultant(**consultant_dummy_data)
+
+            client.send_json(command_data)
+            response = client.recv_json()
         elif command.lower() == "company":
-            consultant_dummy_data = {
-                "name": "Andreas Johansson",
-                "expertise": ["Project Management", "Agile Methodologies", "IT Consulting"],
-                "years_of_experience": 5,
-                "certifications": ["Programming Cert", "IT Cert", "Azure AI-900"],
-                "contact_email": "andreas@johansson.se",
-                "contact_phone": "+46 72 356 77 71"
+            company_dummy_data = Company.Generate()
+
+            command_data = {
+                'command': 'create',
+                'params': {
+                    'collection_name': 'Company',
+                    'document_data': company_dummy_data.to_dict()
+                }
             }
-            Consultant.AddConsultant(**consultant_dummy_data)
+
+            client.send_json(command_data)
+            response = client.recv_json()
         elif command.lower() == "agent":
             manager = AgentManager()
-        elif command.lower() == "status":
-            response = requests.get("http://127.0.0.1:5000/status")
-            if response.status_code == 200:
-                print("Message:", response.json()["message"])
-            else:
-                print(f"Unexpected status code: {response.status_code}")
         elif command.lower() == "domain":
             DomainMain('C:/Users/Mondus/Documents/Länkar.txt')
+        elif command.lower() == "status":
+            response = requests.get("http://127.0.0.1:5000/server-status")
+            print(response.status_code)
+            print(response.text)
         elif command.lower() == "exit":
             break
         else:
