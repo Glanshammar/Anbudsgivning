0b0c1c4e47b41c3322f6c0b6c43f1efe29f36e45
Andreas Johansson <glanshammar70@gmail.com>
2025-04-29 12:03:43 +0200

    Added API functions & JWT

--- Backend/db_app.py
+++ Backend/db_app.py
@@ -1,9 +1,24 @@
+import os
+import sys
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+root_dir = os.path.dirname(current_dir)
+sys.path.insert(0, root_dir)
+sys.path.insert(0, current_dir)
+
 import firebase_admin
 from firebase_admin import credentials, firestore
 from flask import Flask
 from .jwt_authentication import JWT_SECRET_KEY
+import userpaths
 
 db = None
+
+class ApiConfig:
+    def __init__(self, firebase_cred_path=f"{root_dir}/credentials.json", debug=False, secret_key=os.getenv('SECRET_KEY')):
+        self.FIREBASE_CRED_PATH = firebase_cred_path
+        self.DEBUG = debug
+        self.SECRET_KEY = secret_key
 
 def InitDB(cred_path):
     cred = credentials.Certificate(cred_path)
--- Backend/jwt_authentication.py
+++ Backend/jwt_authentication.py
@@ -1,10 +1,20 @@
+import os
+import sys
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+root_dir = os.path.dirname(current_dir)
+sys.path.insert(0, root_dir)
+sys.path.insert(0, current_dir)
+
 import datetime
 import jwt
 from functools import wraps
 from httpcodes import *
 from flask import request
+from flask_jwt_extended import JWTManager, jwt_required, get_jwt_identity, create_access_token
 
-SECRET_KEY = 'super_secret_key'
+JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY")
+
 
 def GenerateJWT(user_id):
     payload = {
@@ -12,11 +22,11 @@ def GenerateJWT(user_id):
         "exp": datetime.datetime.now(datetime.timezone.utc) + datetime.timedelta(minutes=30),
         "iat": datetime.datetime.now(datetime.timezone.utc),
     }
-    return jwt.encode(payload, SECRET_KEY, algorithm="HS256")
+    return jwt.encode(payload, JWT_SECRET_KEY, algorithm="HS256")
 
 def VerifyJWT(token):
     try:
-        payload = jwt.decode(token, SECRET_KEY, algorithms=["HS256"])
+        payload = jwt.decode(token, JWT_SECRET_KEY, algorithms=["HS256"])
         return payload
     except jwt.ExpiredSignatureError:
         return None, "Token expired"
