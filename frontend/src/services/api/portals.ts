47e18b267e97ea597b260535f51a04cb4f83b452
Andraoz85 <andraoz85@gmail.com>
2025-05-20 10:22:33 +0200

    add threading lock in backend to prevent 'race condition' when sending multiple request at the same time to the server

--- frontend/src/services/api/portals.ts
+++ frontend/src/services/api/portals.ts
@@ -7,9 +7,6 @@ export interface Portal {
 export const getPortals = async () => {
   const response = await fetch("http://localhost:5000/api/tender_portals", {
     method: "GET",
-    headers: {
-      "Content-Type": "application/json",
-    },
   });
   const data = await response.json();
   if (!response.ok) {
